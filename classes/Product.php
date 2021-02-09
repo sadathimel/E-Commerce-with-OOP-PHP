9eea3bad0628e99be85f1209ee4a6cb548b9f828
sadat <sadat.himel@gmail.com>
2021-02-09 14:45:11 +0600

    add product list

--- classes/Product.php
+++ classes/Product.php
@@ -57,5 +57,11 @@
 		}
 	    }
 	}
+
+		public function getAllProduct() {
+			$query = "SELECT * FROM tbl_product ORDER BY productId DESC";
+			$result = $this->db->select($query);
+		return $result;
+	}
 }
 ?>
